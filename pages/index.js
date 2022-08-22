11a4cf4216359e5bdd4b74855660f2a9ed4f234e
jatomic <johnatomich@gmail.com>
2022-08-22 14:14:36 -0500

    Monkey Island Theme!

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,51 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Script from 'next/script'
 import Image from 'next/image'
-import Logo from '../public/images/2dPieChart.svg'
+import Logo from '../public/images/niceColors1.svg'
+
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+
+import MyNavbar from './components/MyNavbar'
 
 export default function Home() {
   return (
     <>
-      <div className={styles.container}>
-      {/* <div> */}
         <Head>
           <title>Andrew's Page</title>
           <meta name="description" content="All About Andrew" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/../public/favicon.ico" />
         </Head>
 
+        <MyNavbar />
+
+      <div className={styles.container}>
         <main className={ styles.main }>
-          <h1 className={ styles.title }>ANDREW TOMICH</h1>
+          {/* <h1 className={ styles.indextitle }>ANDREW TOMICH</h1> */}
 
-          <Image src={ Logo } height={ 180 } width={ 180 } />
+          <Image className={ styles.Applogo } src={ Logo } height={ 360 } width={ 360 } />
 
-          <a className='btn btn-primary' href="http://localhost:8888/tree/pages/components/jnb/eda.ipynb" target="_blank">button</a>
 
         </main>
+
+          <Container style={{color: `white`}}>
+            <Row>
+              <Col>
+                <h1>HERE IS SOME NEW TEXT IN COLUMN 1</h1>
+              </Col>
+              <Col>
+                <h1>HERE IS SOME NEW TEXT IN COLUMN 2</h1>
+              </Col>
+              <Col>
+                <h1>HERE IS SOME NEW TEXT IN COLUMN 3</h1>
+              </Col>
+            </Row>
+          </Container>
 
         <footer className={styles.footer}>
         </footer>
       </div>
-      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossOrigin='anonymous'></Script>
-      <Script src="components/navShowHide.js"></Script>
     </>
   )
 }
