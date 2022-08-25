c72cd785c3fd7e62aeeab14ed7c2a2e1d3655987
jatomic <johnatomich@gmail.com>
2022-08-24 23:26:21 -0500

    Create Navbar. Try to hide on scroll

--- pages/components/MyNavbar.js
+++ pages/components/MyNavbar.js
@@ -1,46 +1,82 @@
 import Image from 'next/image'
-import Link from 'next/link'
-import Logo from '../../public/images/2dPieChart.svg'
+import Logo from '../../public/images/niceColors1.svg'
 
-export default function MyNavbar() {
-    return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="/">
-            <Image src={ Logo } height="35" width="35" />
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/album">Album</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><hr class="dropdown-divider" /></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
-              </li>
-            </ul>
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
-    )
+// export default function MyNavbar() {
+//     return (
+//         <nav className="navbar navbar-expand-lg navbar-dark bg-dar" id="mainNav">
+//         <div className="container-fluid">
+//           <a className="navbar-brand" href="/">
+            // <Image src={ Logo } height="30" width="30" />
+//           </a>
+//           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//             <span className="navbar-toggler-icon"></span>
+//           </button>
+//           <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//               <li className="nav-item">
+//                 <a className="nav-link active" aria-current="page" href="/">Home</a>
+//               </li>
+//               <li className="nav-item">
+//                 <a className="nav-link" href="/album">Album</a>
+//               </li>
+//               <li className="nav-item dropdown">
+//                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//                   Dropdown
+//                 </a>
+//                 <ul className="dropdown-menu">
+//                   <li><a className="dropdown-item" href="#">Action</a></li>
+//                   <li><a className="dropdown-item" href="#">Another action</a></li>
+//                   <li><hr className="dropdown-divider" /></li>
+//                   <li><a className="dropdown-item" href="#">Something else here</a></li>
+//                 </ul>
+//               </li>
+//               <li className="nav-item">
+//                 <a className="nav-link disabled">Disabled</a>
+//               </li>
+//             </ul>
+//             <form className="d-flex" role="search">
+//               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+//               <button className="btn btn-outline-success" type="submit">Search</button>
+//             </form>
+//           </div>
+//         </div>
+//       </nav>
+//     )
+// }
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function MyNavbar() {
+  return (
+    <Navbar bg="dark" expand="lg" variant="dark">
+      <Container>
+        <Navbar.Brand href="/">
+            <Image src={ Logo } height="45" width="45" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/album">Album</Nav.Link>
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
+
+export default MyNavbar; 
