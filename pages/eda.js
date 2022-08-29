b6a13a89879e535ea250cf9db4518090988d6913
jatomic <johnatomich@gmail.com>
2022-08-28 23:57:28 -0500

    Address Formatting

---
+++ pages/eda.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Exploratory Data Analysis</title>
+      </Head>
+
+
+        <footer className={styles.footer}>
+        </footer>
+    </>
+  )
+}
